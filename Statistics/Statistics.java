9782bb0296028e1f9ebfefea89ef654b3cebc1ed
Etsub Fikreab <76394016+EtsubFikreab@users.noreply.github.com>
2021-08-04 17:44:01 +0300

    folder package

---
+++ Statistics/Statistics.java
@@ -0,0 +1,14 @@
+/**
+ * All functions in the package are accessible from this class.
+ * 
+ * If you want to create other files within this package make sure that they
+ * are only accessible from this class.
+ * 
+ * When making changes or adding functions make sure your write an example
+ * of how the function responds to the data
+ * i.e. write input data and expected output data 
+ */
+package Statistics;
+public class Statistics{
+    
+}
