1b7a8199d09a0ba73d7e8d3dfecec07136f3069a
Etsub Fikreab <76394016+EtsubFikreab@users.noreply.github.com>
2021-08-08 20:52:37 +0300

    General file

---
+++ General.java
@@ -0,0 +1,9 @@
+/**
+ * class made for testing and making the genaral functions
+ * the functions will be changed to output and input from the monitor(GUI)
+ */
+public class General {
+    public static void main(String[] args){
+        
+    }
+}
