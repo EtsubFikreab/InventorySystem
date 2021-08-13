f6f698dae3ab61e6a594941421908b4db5fed5c8
Etsub Fikreab <76394016+EtsubFikreab@users.noreply.github.com>
2021-08-13 22:29:49 +0300

    classes for the data types improved pictures

--- General.java
+++ General.java
@@ -2,8 +2,75 @@
  * class made for testing and making the genaral functions
  * the functions will be changed to output and input from the monitor(GUI)
  */
+import java.util.Scanner;
 public class General {
     public static void main(String[] args){
         
     }
+    
 }
+class Product {
+    String name;
+    double quantity, priceSold, priceBought;
+    void getProduct(){
+
+    }
+
+}
+//credit: yoni's contribution
+class Sell{
+    int sellID, pID, CID;
+    double quantity, price;
+    String Date;
+}
+class Supplier{
+	int SID;
+	String Sname;
+	String SAddress;
+	int phone;
+	short POBOX;
+	String SEmail;
+	String SDesc;
+}
+ class buy{
+	 int SID;
+	 int StID;
+	 int PID;
+	 int Quantity;
+	 double Price;
+	 String Date;
+ }
+ class sell{
+	 int StID;
+	 int PID;
+	 int CID;
+	 int Quantity;
+	 double Price;
+	 String Date;
+ }
+ class Stuff{
+	 String Fname;
+	 String Lname;
+	 int StID;
+	 String StAddress;
+	 int Stphone;
+	 String StEmail;
+ }
+ class Customer{
+	 String Fname;
+	 String Lname;
+	 int CID; 
+	 String CAddress;
+	 int Cphone;
+	 String CEmail;
+ }
+ class Catagory{
+	 String CatName;
+	 int CatID;
+	 String CatDesc;
+ }
+ class Storage{
+	 String StorageArea;
+	 int StorageID;
+	 String StoDesc;
+ }
