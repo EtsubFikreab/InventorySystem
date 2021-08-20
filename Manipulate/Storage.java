b732fb54d9fc3dddb87c5c56351672ebb95cfc0b
Etsub Fikreab <76394016+EtsubFikreab@users.noreply.github.com>
2021-08-20 12:15:43 +0300

    Storage

--- Manipulate/Storage.java
+++ Manipulate/Storage.java
@@ -1,5 +1,30 @@
 package Manipulate;
 
-public class Storage {
-    
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class Storage extends Connect{
+    String StorageArea;
+	 int StorageID;
+	 String StoDesc;
+     ResultSet rs;
+     public String [] read(){
+        String[] results = new String[3];
+        try{
+            String SQL = "SELECT * FROM storage";    
+            rs = stmt.executeQuery( SQL );
+            //hold all the records from the database table
+            rs.next( );
+            results[0] = Integer.toString(rs.getInt("storageArea"));
+            results[1] = Integer.toString(rs.getInt("storageID"));
+            results[2] = rs.getString("Description");
+        }
+        catch ( SQLException err ) {
+            System.out.println( err.getMessage( ) );
+        }
+        return results;
+     }
+     public void write(String result[]){
+ 
+     }
 }
